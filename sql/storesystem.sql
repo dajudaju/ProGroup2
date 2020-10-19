f8904e08500b7efd0c266d775d6d7f5a4cf7b43d
dell <dell@192.168.2.126>
2020-10-19 09:55:30 +0800

    后台+数据库

--- sql/storesystem.sql
+++ sql/storesystem.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80017
  File Encoding         : 65001
 
- Date: 16/10/2020 20:40:31
+ Date: 19/10/2020 09:54:38
 */
 
 SET NAMES utf8mb4;
@@ -28,11 +28,14 @@ CREATE TABLE `tb_address`  (
   PRIMARY KEY (`a_id`) USING BTREE,
   INDEX `a_cid`(`c_id`) USING BTREE,
   CONSTRAINT `a_cid` FOREIGN KEY (`c_id`) REFERENCES `tb_customer` (`c_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tb_address
 -- ----------------------------
+INSERT INTO `tb_address` VALUES (1, 1, '北京');
+INSERT INTO `tb_address` VALUES (2, 1, '杭州');
+INSERT INTO `tb_address` VALUES (3, 2, '北京');
 
 -- ----------------------------
 -- Table structure for tb_customer
@@ -47,11 +50,13 @@ CREATE TABLE `tb_customer`  (
   `c_state` bit(1) NOT NULL DEFAULT b'0' COMMENT '用户账号状态，0：激活，1：禁用',
   PRIMARY KEY (`c_id`) USING BTREE,
   UNIQUE INDEX `c_name`(`c_name`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tb_customer
 -- ----------------------------
+INSERT INTO `tb_customer` VALUES (1, 'lisa', '123456', b'0', '1111', b'1');
+INSERT INTO `tb_customer` VALUES (2, 'leo', '111111', b'0', '1111', b'0');
 
 -- ----------------------------
 -- Table structure for tb_discuss
@@ -68,11 +73,13 @@ CREATE TABLE `tb_discuss`  (
   INDEX `d_wid`(`w_id`) USING BTREE,
   CONSTRAINT `d_cid` FOREIGN KEY (`c_id`) REFERENCES `tb_customer` (`c_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `d_wid` FOREIGN KEY (`w_id`) REFERENCES `tb_ware` (`w_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tb_discuss
 -- ----------------------------
+INSERT INTO `tb_discuss` VALUES (1, 1, 2, '好用', '谢谢');
+INSERT INTO `tb_discuss` VALUES (2, 2, 1, '很棒', NULL);
 
 -- ----------------------------
 -- Table structure for tb_information
@@ -86,7 +93,7 @@ CREATE TABLE `tb_information`  (
   PRIMARY KEY (`i_id`) USING BTREE,
   INDEX `i_cid`(`c_id`) USING BTREE,
   CONSTRAINT `i_cid` FOREIGN KEY (`c_id`) REFERENCES `tb_customer` (`c_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tb_information
@@ -102,12 +109,12 @@ CREATE TABLE `tb_manager`  (
   `m_password` varchar(20) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '管理员密码，无加密，6~12位',
   PRIMARY KEY (`m_id`) USING BTREE,
   UNIQUE INDEX `m_name`(`m_name`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tb_manager
 -- ----------------------------
-INSERT INTO `tb_manager` VALUES (1, 'admin', '123456');
+INSERT INTO `tb_manager` VALUES (1, 'admin', '111111');
 INSERT INTO `tb_manager` VALUES (2, 'super', '123456');
 
 -- ----------------------------
@@ -133,11 +140,15 @@ CREATE TABLE `tb_order`  (
   CONSTRAINT `o_aid` FOREIGN KEY (`a_id`) REFERENCES `tb_address` (`a_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `o_cid` FOREIGN KEY (`c_id`) REFERENCES `tb_customer` (`c_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `o_wid` FOREIGN KEY (`w_id`) REFERENCES `tb_ware` (`w_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tb_order
 -- ----------------------------
+INSERT INTO `tb_order` VALUES (1, 1, 2, 1, 4234325, '张三', '1111111', 2, 434.00, '2020-10-18 21:09:33', 1);
+INSERT INTO `tb_order` VALUES (2, 1, 1, 2, 456467, '李四', '222222', 1, 222.00, '2020-10-16 21:12:38', 1);
+INSERT INTO `tb_order` VALUES (5, 2, 2, 3, 4535353, '李四', '3434353', 1, 322.00, '2020-10-27 21:15:02', 1);
+INSERT INTO `tb_order` VALUES (6, 2, 5, 3, 5454353, '李四', '434224', 1, 222.00, '2020-10-19 21:17:25', 4);
 
 -- ----------------------------
 -- Table structure for tb_ware
@@ -150,7 +161,7 @@ CREATE TABLE `tb_ware`  (
   `w_brand` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '小飞猪' COMMENT '商品品牌，只有一个值：小飞猪',
   `w_price` double(6, 2) NOT NULL COMMENT '商品单价',
   `w_repertory` int(11) NOT NULL DEFAULT 0 COMMENT '商品库存',
-  `w_url` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '商品图片地址',
+  `w_url` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '商品图片地址',
   `w_material` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '商品材料',
   `w_describe` varchar(300) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '商品描述',
   `w_sale` int(11) NOT NULL DEFAULT 0 COMMENT '商品销量',
@@ -159,11 +170,15 @@ CREATE TABLE `tb_ware`  (
   UNIQUE INDEX `w_name`(`w_name`) USING BTREE,
   INDEX `w_tid`(`t_id`) USING BTREE,
   CONSTRAINT `w_tid` FOREIGN KEY (`t_id`) REFERENCES `tb_waretype` (`t_id`) ON DELETE SET NULL ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tb_ware
 -- ----------------------------
+INSERT INTO `tb_ware` VALUES (1, 1, '双人沙发', '小飞猪', 2000.00, 100, 'ba5fac6a-29cb-45e7-af90-85185f0960c8.jpg', '布料', '很舒服', 2, b'0');
+INSERT INTO `tb_ware` VALUES (2, 2, '木椅', '小飞猪', 500.00, 100, '02950a50-80c3-4b1c-adf2-a94aad1fb728.jpg', '红木', '很不错', 4, b'0');
+INSERT INTO `tb_ware` VALUES (5, 4, '不粘锅', '小飞猪', 233.00, 200, '5018aae1-02c2-4a12-a710-80dda54ac2b2.jpg', '不锈钢', '好用', 3, b'0');
+INSERT INTO `tb_ware` VALUES (6, 1, '沙发', '小飞猪', 222.00, 222, 'a02671aa-c1ed-42d6-b6fb-c22d3a9f93e4.jpg', '木', '舒服', 0, b'0');
 
 -- ----------------------------
 -- Table structure for tb_warecustomer
@@ -194,14 +209,15 @@ CREATE TABLE `tb_waretype`  (
   `t_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '商品分类名称',
   PRIMARY KEY (`t_id`) USING BTREE,
   UNIQUE INDEX `t_id`(`t_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tb_waretype
 -- ----------------------------
 INSERT INTO `tb_waretype` VALUES (1, '沙发');
-INSERT INTO `tb_waretype` VALUES (2, '座椅');
+INSERT INTO `tb_waretype` VALUES (2, '椅子');
 INSERT INTO `tb_waretype` VALUES (3, '餐桌');
 INSERT INTO `tb_waretype` VALUES (4, '厨房用品');
+INSERT INTO `tb_waretype` VALUES (8, '台灯');
 
 SET FOREIGN_KEY_CHECKS = 1;
